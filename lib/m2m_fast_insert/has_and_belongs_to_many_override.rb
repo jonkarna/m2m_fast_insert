7f601a3aa856c99555d7884cb7d8e5adff284260
Jon Karna <ipg49vv2@gmail.com>
2012-08-17 14:38:01 -0500

    Hacked to get passing specs for rails 3.2.8

--- lib/m2m_fast_insert/has_and_belongs_to_many_override.rb
+++ lib/m2m_fast_insert/has_and_belongs_to_many_override.rb
@@ -5,7 +5,11 @@ module M2MFastInsert
       class_eval do
         # Create Method chain if habtm is defined - This is because it goes down to AR::Base
         # and errors because at the time of inclusion, there is none defined
-        alias_method_chain :has_and_belongs_to_many, :fast_inserts if self.method_defined? :has_and_belongs_to_many
+        if self.method_defined? :build
+          alias_method_chain :build, :fast_inserts
+        else
+          raise "wtf"
+        end
       end
     end
 
@@ -13,8 +17,8 @@ module M2MFastInsert
     #
     # name - Plural name of the model we're associating with
     # options - see ActiveRecord docs
-    def has_and_belongs_to_many_with_fast_inserts(name, options = {})
-      has_and_belongs_to_many_without_fast_inserts(name, options)
+    def build_with_fast_inserts
+      build_without_fast_inserts
       define_fast_methods_for_model(name, options)
     end
 
@@ -27,12 +31,16 @@ module M2MFastInsert
     def define_fast_methods_for_model(name, options)
       join_table = options[:join_table]
       join_column_name = name.to_s.downcase.singularize
-      define_method "fast_#{join_column_name}_ids_insert" do |*args|
-        table_name = self.class.table_name.singularize
-        insert = M2MFastInsert::Base.new id, join_column_name, table_name, join_table, *args
-        insert.fast_insert
-      end
+      model.send(:include, Module.new {
+        class_eval <<-RUBY, __FILE__, __LINE__ + 1
+          def fast_#{join_column_name}_ids_insert(*args)
+            table_name = self.class.table_name.singularize
+            insert = M2MFastInsert::Base.new id, #{join_column_name}, table_name, #{join_table}, *args
+            insert.fast_insert
+          end
+        RUBY
+      })
     end
   end
 end
-ActiveRecord::AutosaveAssociation::ClassMethods.send :include, M2MFastInsert::HasAndBelongsToManyOverride
+ActiveRecord::Associations::Builder::HasAndBelongsToMany.send :include, M2MFastInsert::HasAndBelongsToManyOverride
